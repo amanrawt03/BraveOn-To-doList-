5ea312512e6a853d812094ee19cd1ba49006ea32
amanrawt03 <amanrawat3021@gmail.com>
2024-08-06 08:57:55 +0530

    resolved auth error

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import path from 'path'
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
@@ -8,7 +9,8 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000; // Provide a default port
+const port = process.env.PORT || 3000; 
+const __dirname = path.resolve();
 connectDB();
 
 app.use(express.json());
@@ -17,6 +19,15 @@ app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
